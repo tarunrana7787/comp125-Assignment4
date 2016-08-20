993e72b9d102b23a6cb7ee95d112b162199cdcbe
tarunrana7787 <tarunrana7787@gmail.com>
2016-08-20 17:20:23 -0400

    addedBannerjs

---
+++ Script/banner.js
@@ -0,0 +1,16 @@
+/**
+ * author : Tarun Rana
+ * date : 20th August 2016
+ * Student Id : 300871205
+ * course : COMP 125
+ * Purpose : script for banner ad
+ */
+
+var MyCanvas=document.getElementById("canvas");
+var stage;
+
+
+function init(){
+stage = new createjs.Stage(canvas);
+
+}
