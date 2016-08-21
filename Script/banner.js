1d1e44ce70821f4e93924e2b0cbf48845e804cb8
tarunrana7787 <tarunrana7787@gmail.com>
2016-08-20 20:03:35 -0400

    Testing

--- Script/banner.js
+++ Script/banner.js
@@ -5,12 +5,12 @@
  * course : COMP 125
  * Purpose : script for banner ad
  */
-
 var body=document.getElementById("body");
 body.addEventListener("load",init());
 var MyCanvas;
 var stage;
-
+var text;
+var picture;
 /**
  * @ function: init()
  * @ purpose: to handle body load event
@@ -19,13 +19,17 @@ var stage;
 function init(){
 canvas=document.getElementById("canvas");
 stage = new createjs.Stage(canvas);
-var text = new createjs.Text("CreateJs CDN", "25px Verdana", "#000");
-text.x = 5;
-text.y = 220;
+text = new createjs.Text("CreateJs CDN", "25px Verdana", "#000");
+text.x = 0;
+text.y = 345;
 text.regX=250*0.5;
 text.regY=250*0.5;
 stage.addChild(text);
-
+createjs.Ticker.framerate = 25;
+createjs.Ticker.addEventListener("tick", Looper);
+text.addEventListener("click", VisitLink, false);
+picture = new createjs.Bitmap("http://cdn.bleacherreport.net/images/team_logos/150x150/tennis.png");
+stage.addChild(picture);
 }
 /**
  * @ function: Looper()
@@ -33,9 +37,19 @@ stage.addChild(text);
  * @ void
  */
 function Looper(){
-    text.x+=3;
-    if(text.x>400){
+    text.x += 3;
+    // text.y+=3;
+    if(text.x > 400){
         text.x=0;
+        // text.y=0;
     }
     stage.update();
+}
+/**
+ * @ function: VisitLink()
+ * @ pourpose: to visit the link when the text in canvas is clicked
+ * @ void
+ */
+function VisitLink(){
+    window.URL("http://studentweb.cencol.ca/trana5/Assignment3.html");
 }
